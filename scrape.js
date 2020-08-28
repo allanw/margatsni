308f77035d3a09be8810141915318f026e4ef4e4
allanw <al.whatmough@gmail.com>
2020-08-28 16:15:49 +1000

    debugging

--- scrape.js
+++ scrape.js
@@ -48,10 +48,15 @@ const getPosts = maxId => {
   if (maxId)
     url = url2 + `&variables={"id":"${userId}","first":12,"after":"${maxId}"}`
 
-  request(url, { encoding: `utf8` }, (err, res, body) => {
+  request(url, { encoding: `utf8`,
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+            'cache-control': 'max-age=0',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+        }, (err, res, body) => {
     if (err) console.log(`error: ${err}`)
-    console.log(url);
-    console.log(body);
     if (maxId) {
       body = JSON.parse(body).data
     } else {
